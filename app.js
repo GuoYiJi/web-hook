d74eb2ecd117e0da06f0eef23a10908d9565f6ff
Shuai Guo <735183844@qq.com>
2017-02-09 15:26:22 +0800

    fix

--- app.js
+++ app.js
@@ -5,50 +5,53 @@ const PORT = 9988
   , PATH = '../html'
 
 var deployServer = http.createServer(function(request, response) {
-  if (request.url.search(/deploy\/?$/i) > 0) {
+  const url = request.query.url
+  switch (url) {
+    case 'test-demo':
+      var commands = [
+        'cd /bookdata/guoyj/test-demo',
+        'git pull origin master'
+      ].join(' && ')
 
-    var commands = [
-      'cd ' + PATH,
-      'git pull'
-    ].join(' && ')
-
-    exec(commands, function(err, out, code) {
-      if (err instanceof Error) {
-        response.writeHead(500)
-        response.end('Server Internal Error.')
-        throw err
-      }
-      process.stderr.write(err)
-      process.stdout.write(out)
-      response.writeHead(200)
-      response.end('Deploy Done.')
-
-    })
-
-  } else {
-
-    response.writeHead(404)
-    response.end('Not Found.')
-
+      exec(commands, function(err, out, code) {
+        if (err instanceof Error) {
+          response.writeHead(500)
+          response.end('Server Internal Error.')
+          throw err
+        }
+        process.stderr.write(err)
+        process.stdout.write(out)
+        response.writeHead(200)
+        response.end('Build Done.')
+      })
+      break
+    default:
+      response.writeHead(404)
+      response.end('Not Found.')
   }
-  if (request.url.search(/test-demo\/?$/i) > 0) {
-    var commands = [
-      'cd /bookdata/guoyj/test-demo',
-      'git pull origin master'
-    ].join(' && ')
+  // if (request.url.search(/deploy\/?$/i) > 0) {
 
-    exec(commands, function(err, out, code) {
-      if (err instanceof Error) {
-        response.writeHead(500)
-        response.end('Server Internal Error.')
-        throw err
-      }
-      process.stderr.write(err)
-      process.stdout.write(out)
-      response.writeHead(200)
-      response.end('Build Done.')
-    })
-  }
+  //   var commands = [
+  //     'cd ' + PATH,
+  //     'git pull'
+  //   ].join(' && ')
+
+  //   exec(commands, function(err, out, code) {
+  //     if (err instanceof Error) {
+  //       response.writeHead(500)
+  //       response.end('Server Internal Error.')
+  //       throw err
+  //     }
+  //     process.stderr.write(err)
+  //     process.stdout.write(out)
+  //     response.writeHead(200)
+  //     response.end('Deploy Done.')
+
+  //   })
+
+  // } else {
+
+  // }
 })
 
 deployServer.listen(PORT)
